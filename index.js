acfd14889a3dce0f7007b99bd47f8449f9e8e5e7
martinovicivica22 <97768436+martinovicivica22@users.noreply.github.com>
2022-05-13 14:29:22 -0600

    make it better

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ const toDosRoute = require("./routes/todos");
 //middlewear that allows you pass json
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+const reactBuild = path.join(__dirname, "client", "build");
+app.use(express.static(reactBuild));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(__dirname, reactBuild, "index.html"));
 });
 
 // allows to use and read cookies during requests
